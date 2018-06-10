62e993fe8461989db7aaa1eea02a3dfbeb0ab298
Borengar <kramer.martin.km@gmail.com>
2018-06-10 15:29:01 +0200

    first menu design

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,49 @@ md-app(md-waterfall md-mode="fixed")
 	md-app-toolbar.md-primary
 		span.md-title.flex {{ routeName }}
 		discord-profile(v-bind:profile="discordProfile")
+	md-app-drawer(md-permanent="full")
+		md-toolbar.md-transparent(md-elevation="0") Navigation
+		md-list
+			md-list-item(to="/registration") Registration
+			md-list-item(md-expand)
+				span.md-list-item-text Stats
+				md-list(slot="md-expand")
+					md-list-item.md-insert(to="stats/home") Home
+					md-list-item.md-insert(to="stats/lobbies") Lobbies
+					md-list-item.md-insert(to="stats/mappools") Mappools
+					md-list-item.md-insert(to="stats/players") Players
+			md-list-item(md-expand)
+				span.md-list-item-text Admin
+				md-list(slot="md-expand")
+					md-list-item.md-insert(to="/admin/bracket") Bracket
+					md-list-item.md-insert(to="/admin/lobbies") Lobbies
+					md-list-item.md-insert(to="/admin/mappoolers") Mappoolers
+					md-list-item.md-insert(to="/admin/players") Players
+					md-list-item.md-insert(to="/admin/registrations") Registrations
+					md-list-item.md-insert(to="/admin/discordroles") Discord Roles
+					md-list-item.md-insert(to="/admin/spreadsheets") Spreadsheets
+					md-list-item.md-insert(to="/admin/tiers") Tiers
+					md-list-item.md-insert(to="/admin/timeslots") Timeslots
+			md-list-item(md-expand)
+				span.md-list-item-text Headpooler
+				md-list(slot="md-expand")
+					md-list-item.md-insert(to="/headpooler/mappools") Mappools
+					md-list-item.md-insert(to="/headpooler/feedback") Feedback
+			md-list-item(md-expand)
+				span.md-list-item-text Mappooler
+				md-list(slot="md-expand")
+					md-list-item.md-insert(to="/mappooler/mappools") Mappools
+					md-list-item.md-insert(to="/mappooler/feedback") Feedback
+			md-list-item(md-expand)
+				span.md-list-item-text Referee
+				md-list(slot="md-expand")
+					md-list-item.md-insert(to="/referee/lobbies") Lobbies
+			md-list-item(md-expand)
+				span.md-list-item-text Player
+				md-list(slot="md-expand")
+					md-list-item.md-insert(to="/player/home") Home
+					md-list-item.md-insert(to="/player/lobbies") Lobbies
+					md-list-item.md-insert(to="/player/mappools") Mappools
 	md-app-content
 		router-view
 </template>
