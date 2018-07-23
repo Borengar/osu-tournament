85f4c580e59cf75bb01826a1fbe1c4e83d67168c
Borengar <kramer.martin.km@gmail.com>
2018-07-23 22:16:24 +0200

    headpooler home page

---
+++ src/frontend/headpooler/HeadpoolerHome.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.wrapper
+	v-btn.big-button(to="/headpooler/mappools") Mappools
+	v-btn.big-button(to="/headpooler/feedback") Feedback
+</template>
+
+<script>
+export default {
+	name: 'HeadpoolerHome'
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper
+	flex-direction row
+	flex-wrap wrap
+.big-button
+	width 200px
+	height 200px
+</style>
