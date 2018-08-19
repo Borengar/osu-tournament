d0ce0162bc07a403bf5195211ecffe329163cb44
Borengar <kramer.martin.km@gmail.com>
2018-08-19 16:20:17 +0200

    AdminRegistration: use vuetify layout

--- src/frontend/admin/AdminRegistrations.vue
+++ src/frontend/admin/AdminRegistrations.vue
@@ -1,60 +1,61 @@
 <template lang="pug">
-.wrapper
-	.settings-wrapper
+v-layout(column)
+	div
 		h2 Settings
 		v-checkbox(label="Active" v-model="settings.active")
-		.horizontal
-			v-menu(ref="openDate"  :close-on-content-click="false" v-model="openDate"  :nudge-right="40"  :return-value.sync="settings.openDate" lazy transition="scale-transition" offset-y full-width min-width="290px")
+		v-layout(row)
+			v-menu.mr-4(ref="openDate"  :close-on-content-click="false" v-model="openDate"  :nudge-right="40"  :return-value.sync="settings.openDate" lazy transition="scale-transition" offset-y full-width min-width="290px")
 				v-text-field(slot="activator" v-model="settings.openDate" label="Active from date" readonly prepend-icon="event")
 				v-date-picker(v-model="settings.openDate" no-title scrollable)
 					v-spacer
 					v-btn(flat color="primary" @click="openDate = false") Cancel
 					v-btn(flat color="primary" @click="$refs.openDate.save(settings.openDate)") OK
-			v-menu.time-menu(ref="openTime"  :close-on-content-click="false" v-model="openTime"  :nudge-right="40"  :return-value.sync="settings.openTime" lazy transition="scale-transition" offset-y full-width min-width="290px")
+			v-menu(ref="openTime"  :close-on-content-click="false" v-model="openTime"  :nudge-right="40"  :return-value.sync="settings.openTime" lazy transition="scale-transition" offset-y full-width min-width="290px")
 				v-text-field(slot="activator" v-model="settings.openTime" label="Active from time" readonly prepend-icon="access_time")
 				v-time-picker(v-model="settings.openTime" format="24hr" @change="$refs.openTime.save(settings.openTime)")
-		.horizontal
-			v-menu(ref="closeDate"  :close-on-content-click="false" v-model="closeDate"  :nudge-right="40"  :return-value.sync="settings.closeDate" lazy transition="scale-transition" offset-y full-width min-width="290px")
+		v-layout(row)
+			v-menu.mr-4(ref="closeDate"  :close-on-content-click="false" v-model="closeDate"  :nudge-right="40"  :return-value.sync="settings.closeDate" lazy transition="scale-transition" offset-y full-width min-width="290px")
 				v-text-field(slot="activator" v-model="settings.closeDate" label="Active to date" readonly prepend-icon="event")
 				v-date-picker(v-model="settings.closeDate" no-title scrollable)
 					v-spacer
 					v-btn(flat color="primary" @click="closeDate = false") Cancel
 					v-btn(flat color="primary" @click="$refs.closeDate.save(settings.closeDate)") OK
-			v-menu.time-menu(ref="closeTime"  :close-on-content-click="false" v-model="closeTime"  :nudge-right="40"  :return-value.sync="settings.closeTime" lazy transition="scale-transition" offset-y full-width min-width="290px")
+			v-menu(ref="closeTime"  :close-on-content-click="false" v-model="closeTime"  :nudge-right="40"  :return-value.sync="settings.closeTime" lazy transition="scale-transition" offset-y full-width min-width="290px")
 				v-text-field(slot="activator" v-model="settings.closeTime" label="Active to time" readonly prepend-icon="access_time")
 				v-time-picker(v-model="settings.closeTime" format="24hr" @change="$refs.closeTime.save(settings.closeTime)")
 		v-btn(@click="saveSettings" color="success") Save
-	.registrations-wrapper
-		.list-wrapper
-			h2 Registrations
-			v-data-table.elevation-1(:items="registrations" item-key="_id")
-				template(slot="headers" slot-scope="props")
-					tr
-						th(align="left") osu!
-						th(align="left") Discord
-						th(align="right") Actions
-				template(slot="items" slot-scope="props")
-					tr
-						td.text-xs-left {{ props.item.osu.username }}
-						td.text-xs-left {{ props.item.discord.username }}\#{{props.item.discord.discriminator}}
-						td.text-xs-right
-							v-icon(small @click="editRegistration(props.item)") edit
-		.edit-wrapper(v-if="editVisible")
-			h2 Edit registration
-			h3 Discord
-			discord-profile(:profile="registration.discord")
-			v-btn(@click="showChangeDiscordAccount" color="success") Change discord account
-			h3 osu
-			osu-profile(:profile="registration.osu")
-			v-btn(@click="showDeleteRegistration" color="error") Delete registration
-			v-btn(@click="cancel") Cancel
-	.transform-wrapper
-		h2 Accept registrations
-		v-btn(@click="createPlayers" color="success"  :disabled="createPlayersDisabled") Registrations -> Players
+	div
+		v-layout(row).mt-2
+			.list-wrapper
+				h2 Registrations
+				v-data-table.elevation-1.mt-1(:items="registrations" item-key="_id")
+					template(slot="headers" slot-scope="props")
+						tr
+							th(align="left") osu!
+							th(align="left") Discord
+							th(align="right") Actions
+					template(slot="items" slot-scope="props")
+						tr
+							td.text-xs-left {{ props.item.osu.username }}
+							td.text-xs-left {{ props.item.discord.username }}\#{{props.item.discord.discriminator}}
+							td.text-xs-right
+								v-icon(small @click="editRegistration(props.item)") edit
+			.ml-4(v-if="editVisible")
+				v-layout(column)
+					h2 Edit registration
+					h3 Discord
+					discord-profile(:profile="registration.discord")
+					v-btn.mx-0(@click="showChangeDiscordAccount" color="success") Change discord account
+					h3 osu
+					osu-profile(:profile="registration.osu")
+					v-btn.mx-0(@click="showDeleteRegistration" color="error") Delete registration
+					v-btn.mx-0(@click="cancel") Cancel
+	h2.mt-2 Accept registrations
+	v-btn.ml-0.create-button(@click="createPlayers" color="success"  :disabled="createPlayersDisabled") Registrations -> Players
 	v-dialog(v-model="changeDiscordAccountDialogVisible" max-width="400")
 		v-card
 			v-card-title.headline Change discord account
-			v-autocomplete.dialog-autocomplete(v-model="targetDiscordAccount"  :items="discordMembers" label="Discord account" item-text="name" item-value="id")
+			v-autocomplete.mx-4(v-model="targetDiscordAccount"  :items="discordMembers" label="Discord account" item-text="name" item-value="id")
 			v-card-actions
 				v-spacer
 				v-btn(flat @click="changeDiscordAccountDialogVisible = false") Cancel
@@ -170,30 +171,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.wrapper
-	display flex
-	flex-direction column
-.horizontal
-	display flex
-	flex-direction row
-.time-menu
-	margin-left 20px
-.registrations-wrapper
-	display flex
-	flex-direction row
-	margin-top 20px
-.list-wrapper
-	width 500px
-.edit-wrapper
-	display flex
-	flex-direction column
-	margin-left 20px
-.transform-wrapper
-	display flex
-	flex-direction column
-	width 500px
-	margin-top 20px
-.dialog-autocomplete
-	margin-left 10px
-	margin-right 10px
+.list-wrapper, .create-button
+	max-width 500px
 </style>
