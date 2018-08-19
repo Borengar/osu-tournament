5647a06dc866db0d9e141d2bee49ec989db8530b
Borengar <kramer.martin.km@gmail.com>
2018-08-19 16:25:41 +0200

    AdminRole: use vuetify layout

--- src/frontend/admin/AdminRoles.vue
+++ src/frontend/admin/AdminRoles.vue
@@ -1,56 +1,58 @@
 <template lang="pug">
-.wrapper
-	.list-wrapper
-		h2 Roles
-		v-data-table.elevation-1(:items="roles" item-key="_id")
-			template(slot="headers" slot-scope="props")
-				tr
-					th(align="left") Name
-					th(align="right") Actions
-			template(slot="items" slot-scope="props")
-				tr
-					td.text-xs-left {{ props.item.name }}
-					td.text-xs-right
-						v-icon.mr-2(small @click="editRole(props.item)" :disabled="editVisible") edit
-						v-icon(small @click="showDeleteDialog(props.item)" :disabled="editVisible") delete
-		v-btn(@click="createRole" color="success") New role
-		v-btn(@click="refreshDiscordRoles") Refresh Discord roles
-	v-form.edit-wrapper(v-if="editVisible" v-model="editValid")
-		h2 {{ editHeader }}
-		v-text-field(label="Name" v-model="role.name" :rules="[rules.required, rules.duplicate]")
-		v-select(label="Discord role" v-model="role.discordRole" :items="discordRoles" item-text="name" item-value="id")
-		h3 Player
-		v-checkbox(label="Is player" v-model="role.permissions.player")
-		h3 Referee
-		v-checkbox(label="Is referee" v-model="role.permissions.referee")
-		h3 Mappooler
-		v-checkbox(label="Is mappooler" v-model="role.permissions.mappooler")
-		v-data-table.elevation-1(v-if="role.permissions.mappooler" :items="tiers" item-key="_id" v-model="role.permissions.mappoolerTiers" select-all)
-			template(slot="headers" slot-scope="props")
-				tr
-					th
-						v-checkbox(:input-value="props.all" primary hide-details @click.native="toggleMappoolerTiers")
-					th(align="left") Name
-			template(slot="items" slot-scope="props")
-				tr
-					td
-						v-checkbox(v-model="props.selected" primary hide-details)
-					td.text-xs-left {{ props.item.name }}
-		h3 Headpooler
-		v-checkbox(label="Is headpooler" v-model="role.permissions.headpooler")
-		h3 Admin
-		v-checkbox(label="Edit availability" v-model="role.permissions.admin.availability")
-		v-checkbox(label="Bracket setup" v-model="role.permissions.admin.bracket")
-		v-checkbox(label="Scheduling" v-model="role.permissions.admin.lobbies")
-		v-checkbox(label="Edit mappoolers" v-model="role.permissions.admin.mappoolers")
-		v-checkbox(label="Edit players" v-model="role.permissions.admin.players")
-		v-checkbox(label="Edit registrations" v-model="role.permissions.admin.registrations")
-		v-checkbox(label="Edit roles" v-model="role.permissions.admin.roles")
-		v-checkbox(label="Edit tiers" v-model="role.permissions.admin.tiers")
-		v-checkbox(label="Edit timeslots" v-model="role.permissions.admin.timeslots")
-		.horizontal
-			v-btn(@click="cancel") Cancel
-			v-btn(@click="saveRole" color="success" :disabled="!editValid || isSuperuser") Save
+v-layout(row)
+	v-flex(lg6).mr-5
+		v-layout(column)
+			h2 Roles
+			v-data-table.elevation-1(:items="roles" item-key="_id")
+				template(slot="headers" slot-scope="props")
+					tr
+						th(align="left") Name
+						th(align="right") Actions
+				template(slot="items" slot-scope="props")
+					tr
+						td.text-xs-left {{ props.item.name }}
+						td.text-xs-right
+							v-icon.mr-2(small @click="editRole(props.item)" :disabled="editVisible") edit
+							v-icon(small @click="showDeleteDialog(props.item)" :disabled="editVisible") delete
+			v-btn.mx-0(@click="createRole" color="success") New role
+			v-btn.mx-0(@click="refreshDiscordRoles") Refresh Discord roles
+	v-flex(lg6)
+		v-layout(column v-if="editVisible")
+			v-form(v-model="editValid")
+				h2 {{ editHeader }}
+				v-text-field(label="Name" v-model="role.name" :rules="[rules.required, rules.duplicate]")
+				v-select(label="Discord role" v-model="role.discordRole" :items="discordRoles" item-text="name" item-value="id")
+				h3 Player
+				v-checkbox(label="Is player" v-model="role.permissions.player")
+				h3 Referee
+				v-checkbox(label="Is referee" v-model="role.permissions.referee")
+				h3 Mappooler
+				v-checkbox(label="Is mappooler" v-model="role.permissions.mappooler")
+				v-data-table.elevation-1(v-if="role.permissions.mappooler" :items="tiers" item-key="_id" v-model="role.permissions.mappoolerTiers" select-all)
+					template(slot="headers" slot-scope="props")
+						tr
+							th
+								v-checkbox(:input-value="props.all" primary hide-details @click.native="toggleMappoolerTiers")
+							th(align="left") Name
+					template(slot="items" slot-scope="props")
+						tr
+							td
+								v-checkbox(v-model="props.selected" primary hide-details)
+							td.text-xs-left {{ props.item.name }}
+				h3 Headpooler
+				v-checkbox(label="Is headpooler" v-model="role.permissions.headpooler")
+				h3 Admin
+				v-checkbox(label="Edit availability" v-model="role.permissions.admin.availability")
+				v-checkbox(label="Bracket setup" v-model="role.permissions.admin.bracket")
+				v-checkbox(label="Scheduling" v-model="role.permissions.admin.lobbies")
+				v-checkbox(label="Edit mappoolers" v-model="role.permissions.admin.mappoolers")
+				v-checkbox(label="Edit players" v-model="role.permissions.admin.players")
+				v-checkbox(label="Edit registrations" v-model="role.permissions.admin.registrations")
+				v-checkbox(label="Edit roles" v-model="role.permissions.admin.roles")
+				v-checkbox(label="Edit tiers" v-model="role.permissions.admin.tiers")
+				v-checkbox(label="Edit timeslots" v-model="role.permissions.admin.timeslots")
+				v-btn(@click="cancel") Cancel
+				v-btn(@click="saveRole" color="success" :disabled="!editValid || isSuperuser") Save
 	v-dialog(v-model="deleteDialogVisible" max-width="300")
 		v-card
 			v-card-title.headline Delete this role?
@@ -233,21 +235,4 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.wrapper
-	display flex
-	flex-direction row
-.list-wrapper
-	display flex
-	flex-direction column
-	width 500px
-.edit-wrapper
-	display flex
-	flex-direction column
-	width 500px
-	margin-left 20px
-.new-button
-	width 150px
-.horizontal
-	display flex
-	flex-direction row
 </style>
