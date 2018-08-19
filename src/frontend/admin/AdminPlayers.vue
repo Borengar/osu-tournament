afed99b530247632230ac94f28230f84e7f430ac
Borengar <kramer.martin.km@gmail.com>
2018-08-19 16:04:00 +0200

    AdminPlayer: use vuetify layout

--- src/frontend/admin/AdminPlayers.vue
+++ src/frontend/admin/AdminPlayers.vue
@@ -1,34 +1,38 @@
 <template lang="pug">
-.wrapper
+v-layout(column justify-start)
 	h2 Filter
-	.filter-wrapper
-		v-select.filter(label="Tier" v-model="filterTier"  :items="tiers" item-text="name" item-value="_id" clearable)
-		v-text-field.filter(label="Search" v-model="filterSearch" append-icon="search")
-	.list-and-detail-wrapper
-		.list-wrapper
-			h2 Player List
-			v-data-table.elevation-1(:items="filteredPlayers" item-key="_id")
-				template(slot="headers" slot-scope="props")
-					tr
-						th(align="left") osu!
-						th(align="left") Discord
-						th(align="right") Actions
-				template(slot="items" slot-scope="props")
-					tr
-						td.text-xs-left {{ props.item.osu.username }}
-						td.text-xs-left {{ props.item.discord.username + '#' + props.item.discord.discriminator }}
-						td.text-xs-right
-							v-icon(small @click="showStats(props.item)") search
-		.detail-wrapper(v-if="player._id")
-			h2 Player Details
-			h3 Discord
-			discord-profile(:profile="player.discord")
-			h3 osu
-			osu-profile(:profile="player.osu")
-			h3 Tier
-			div {{ player.player.tier.name }}
-			h3 Availability
-			div(v-for="time in player.availability") {{ time.day + ' ' + time.time }}
+	div
+		v-layout(row justify-start)
+			v-select.mr-2.filter(label="Tier" v-model="filterTier"  :items="tiers" item-text="name" item-value="_id" clearable)
+			v-text-field.filter(label="Search" v-model="filterSearch" append-icon="search")
+	div
+		v-layout(row)
+			v-flex(lg6).mr-5
+				v-layout(column)
+					h2 Player List
+					v-data-table.elevation-1(:items="filteredPlayers" item-key="_id")
+						template(slot="headers" slot-scope="props")
+							tr
+								th(align="left") osu!
+								th(align="left") Discord
+								th(align="right") Actions
+						template(slot="items" slot-scope="props")
+							tr
+								td.text-xs-left {{ props.item.osu.username }}
+								td.text-xs-left {{ props.item.discord.username + '#' + props.item.discord.discriminator }}
+								td.text-xs-right
+									v-icon(small @click="showStats(props.item)") search
+			v-flex(lg6)
+				v-layout(column v-if="player._id")
+					h2 Player Details
+					h3 Discord
+					discord-profile(:profile="player.discord")
+					h3 osu
+					osu-profile(:profile="player.osu")
+					h3 Tier
+					div {{ player.player.tier.name }}
+					h3 Availability
+					div(v-for="time in player.availability") {{ time.day + ' ' + time.time }}
 </template>
 
 <script>
@@ -70,25 +74,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.wrapper
-	display flex
-	flex-direction column
-.filter-wrapper
-	display flex
-	flex-direction row
 .filter
 	max-width 300px
-	margin-right 10px
-.list-and-detail-wrapper
-	display flex
-	flex-direction row
-.list-wrapper
-	display flex
-	flex-direction column
-	width 500px
-.detail-wrapper
-	display flex
-	flex-direction column
-	width 500px
-	margin-left 20px
 </style>
